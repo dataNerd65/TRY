33a0a264e5d1e5d79ae57b8df196880fd3723d6e
dataNerd65 <pkiarie811@gmail.com>
2025-10-20 21:53:51 +0300

    some additional on manual reverse

--- TRY_CodeForces/reversePO1.cpp
+++ TRY_CodeForces/reversePO1.cpp
@@ -2,6 +2,12 @@
 #include<vector>
 #include<algorithm>
 
+void manual_reverse(std::vector<int>& vec){
+    for(int j = vec.size() - 1, i = 0; i < j; i++, j--){
+        std::swap(vec[i], vec[j]);
+    }
+}
+
 int main(){
     int n;
     std::vector<int> vec;
@@ -12,7 +18,9 @@ int main(){
         vec.push_back(x);
     }
     // reverse 
-    std::reverse(vec.begin(), vec.end());
+    //std::reverse(vec.begin(), vec.end());
+    // manual reverse
+    manual_reverse(vec);
     for(int num : vec){
         std::cout<< num <<" ";
     }
