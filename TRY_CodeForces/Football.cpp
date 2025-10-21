7c0618929366409ef7b375d9aced203d078339e4
dataNerd65 <pkiarie811@gmail.com>
2025-10-21 17:09:00 +0300

    another soln for football

---
+++ TRY_CodeForces/Football.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::string gamers;
+    std::cin>> gamers;
+
+    char prev = gamers[0];
+    int count = 0;
+
+    for(char& c: gamers){
+        if(c == prev){
+            count += 1;
+            if(count >= 7){
+                std::cout<<"YES\n";
+                return 0;
+            }else{
+                count = 1;
+                prev = c;
+            }
+        }
+    }
+    std::cout<<"NO\n";
+    return 0;
+}
