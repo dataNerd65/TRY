df9a65a19f6e0642e68cfb1f3a2dfd3780eb5c0e
dataNerd65 <pkiarie811@gmail.com>
2025-10-21 08:48:28 +0300

    PO2 Candies done

---
+++ TRY_CodeForces/po2Candies.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+int main(){
+
+    int n;
+    std::cin>>n;
+    // std::vector<int> vec;
+    // while(n--){
+    //     int c;
+    //     std::cin>> c;
+    //     vec.push_back(c);
+        
+    // }
+    // prealocation a  bit faster
+    std::vector<int> vec(n); // creates a vector with length n but filled with 0s, so preallocate
+    for(int i =0; i < vec.size(); ++i){
+        std::cin>> vec[i];
+    }
+    int x, z;
+    std::cin>>x>>z;
+    int sum = 0;
+    for(int i = x; i < z + 1; i++ ){
+        sum += vec[i];
+    }
+    std::cout<<sum<<'\n';
+    return 0;
+}
