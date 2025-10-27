661db9f272759adb177bba26b3dfa89f3fc6142c
dataNerd65 <pkiarie811@gmail.com>
2025-10-27 17:06:36 +0300

    solved problem

---
+++ TRY_CodeForces/basicstypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	// soln here
+	int i; long long l; char c; float f; double d; //declaration is a statement
+	std::cin>>i>>l>>c>>f>>d;
+	std::cout<<i<<"\n"<<l<<"\n"<<c<<"\n"<<f<<"\n"<<d<<"\n";
+
+	return 0;
+}
