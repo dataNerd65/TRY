673f392b1c66f028b31ef4993bf9f67399ddae54
dataNerd65 <pkiarie811@gmail.com>
2025-10-28 11:48:04 +0300

    EASY PEASY

---
+++ TRY_CodeForces/sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	// soln here
+	int a, b;
+	std::cin>>a>>b;
+
+	std::cout<<(a) + (b)<<"\n";
+
+	return 0;
+}
