54c2bda0565cb2882c8b5219759a6b30e9e4014f
dataNerd65 <pkiarie811@gmail.com>
2025-11-02 10:48:42 +0300

    new

---
+++ TRY_CodeForces/new.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	// soln here
+	int n;
+	std::cin>>n;
+
+	for(int i = 1; i <= n; ++i){
+		if(n % i == 0){
+			std::cout<<i<<"\n";
+		}
+	}
+
+	return 0;
+}
