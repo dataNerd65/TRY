8aab25a34d7b648bd08dbb52bed3c6a5eab69335
dataNerd65 <pkiarie811@gmail.com>
2025-11-04 11:23:03 +0300

    redoing to master

---
+++ TRY_CodeForces/p22primesieve.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+std::vector<int> sieve(int n){
+	std::vector<bool> prime(n, true);
+	prime[0] = prime[1] = false;
+
+	for(int p = 2; p * p <= n; ++p){
+		if(prime[p]){
+			for(int i = p * p; i <= n; i += p){
+				prime[i] = false;
+			}
+		}
+	}
+	std::vector<int> primes;
+
+	for(int i = 2; i <= n; ++i){
+		if(prime[i]) primes.push_back(i);
+	}
+	return primes;
+}
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	// soln here
+	int n;
+	std::cin>>n;
+
+	std::vector<int> res = sieve(n);
+
+	std::cout<<res.size()<<"\n";
+
+	for(int i = 0; i < res.size(); ++i){
+		std::cout<<res[i]<<" ";
+	}
+	std::cout<<"\n";
+
+	return 0;
+}
