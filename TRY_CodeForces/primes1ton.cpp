3ee45e73629871cb8bccb023d3c34cd573ea30ba
dataNerd65 <pkiarie811@gmail.com>
2025-11-06 15:58:32 +0300

    sieve of eratosthenes

---
+++ TRY_CodeForces/primes1ton.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+std::vector<int> sieve(int n){
+	std::vector<bool> prime(n, true);
+	prime[0] = prime[1] = false;
+
+	for(int p = 2; p * p <= n; ++p){
+		if(prime[p]){
+			for(int i = p * p; i <= n; i += p){
+				prime[i] = false;
+			}
+		}
+	}
+
+	std::vector<int> primes;
+	for(int i = 0; i <= n; ++i){
+		if(prime[i]) primes.push_back(i);
+	}
+	return primes;
+}
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	// soln here
+	int n;
+	std::cin>>n;
+
+	std::vector<int> res = sieve(n);
+
+	for(int i : res){
+		std::cout<<i<<" ";
+	}
+	std::cout<<"\n";
+
+	return 0;
+}
