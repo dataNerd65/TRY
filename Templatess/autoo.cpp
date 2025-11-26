d8bf92a25a9dc6125e951aae67dfb39f0d1a2699
dataNerd65 <pkiarie811@gmail.com>
2025-11-26 13:52:10 +0300

    some changes

--- Templatess/autoo.cpp
+++ Templatess/autoo.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 
-int main(){
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	// soln here
-
-	return 0;
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    // soln here 
+    
+    return 0;
 }
